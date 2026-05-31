51ed4b4aa9f4c88eed60fb9e73e95cfe68435b6f
Petrokolus <peter.gisholt.dev@gmail.com>
2026-05-31 19:26:49 +0200

    style (font): Tilpasse font og tekststørrelse

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto, Roboto_Mono } from "next/font/google";
 import { SideNav } from "@/components/side-nav";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
+  variable: "--font-sans",
   subsets: ["latin"],
+  weight: ["400", "500", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+  variable: "--font-mono",
   subsets: ["latin"],
 });
 
@@ -27,7 +28,7 @@ export default function RootLayout({
   return (
     <html
       lang="no"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${roboto.variable} ${robotoMono.variable} text-md h-full antialiased`}
     >
       <body className="min-h-full">
         <SidebarProvider>

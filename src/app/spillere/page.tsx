428e3d40118dab55a40bef6d492f20ebe10e3dec
Petrokolus <143998525+Petrokolus@users.noreply.github.com>
2026-06-16 16:32:10 +0200

    feature: legge til oppgave 3, samt legge på mer styling på readme (#6)
    * feature: legge til oppgave 3, samt legge på mer styling på readme
    
    * refactor: små korrigeringer på oppgave 1, 2, og 3 for å få mer sammenhengende flyt
    
    * chore (quality): format, lint og build validation
    
    * refactor: endelige korrigeringer
    
    ---------
    
    Co-authored-by: Peter Andreas Gisholt <peter.a.gisholt@twoday.com>

--- src/app/spillere/page.tsx
+++ src/app/spillere/page.tsx
@@ -2,6 +2,7 @@ export default async function SpillerePage() {
   return (
     <div className="max-w-4xl p-8">
       {/* Oppgave 1a - LEGG TIL EN OVERSKRIFT HER */}
+      {/* Oppgave 3a - LEGG TIL EN LENKE TIL /spillere/opprett HER */}
       <p>Her var det ganske tomt foreløpig!</p>
     </div>
   );

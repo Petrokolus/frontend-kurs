6bc386a5ea5936a2975b34822f9b97beb25af4b4
Petrokolus <peter.gisholt.dev@gmail.com>
2026-06-21 21:51:26 +0200

    refactor: korrigere spillercard posisjon

--- README.md
+++ README.md
@@ -459,9 +459,9 @@ export default async function SpillerePage() {
     <div className="max-w-4xl p-8">
       <div className="mb-6 flex items-center justify-between">
         <h1 className="text-3xl font-bold">Spillere</h1>
-        <SpillerCard />
         {/* Oppgave 3a - LEGG TIL EN LENKE TIL /spillere/opprett HER */}
       </div>
+      <SpillerCard />
     </div>
   );
 }
--- src/app/spillere/page.tsx
+++ src/app/spillere/page.tsx
@@ -3,9 +3,9 @@ export default async function SpillerePage() {
     <div className="max-w-4xl p-8">
       <div className="mb-6 flex items-center justify-between">
         {/* Oppgave 1a - LEGG TIL EN OVERSKRIFT HER */}
-        {/* Oppgave 1b - LEGG TIL ET SPILLERCARD HER */}
         {/* Oppgave 3a - LEGG TIL EN LENKE TIL /spillere/opprett HER */}
       </div>
+      {/* Oppgave 1b - LEGG TIL ET SPILLERCARD HER */}
       <p>Her var det ganske tomt foreløpig!</p>
     </div>
   );

7d3a6b71298b7521c03eacfd2b2c784a9e85504c
Petrokolus <peter.gisholt.dev@gmail.com>
2026-06-21 22:25:26 +0200

    refactor: oppdatere eslint for å hindre kjedelige fallgruver

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,34 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "no-restricted-globals": [
+        "warn",
+        {
+          name: "Image",
+          message: "Importer Image fra 'next/image': import Image from 'next/image'",
+        },
+      ],
+      "no-restricted-imports": [
+        "warn",
+        {
+          paths: [
+            {
+              name: "lucide-react",
+              importNames: ["Link"],
+              message: "Importer Link fra 'next/link', ikke lucide-react: import Link from 'next/link'",
+            },
+            {
+              name: "react",
+              importNames: ["Link"],
+              message: "Importer Link fra 'next/link', ikke react: import Link from 'next/link'",
+            },
+          ],
+        },
+      ],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:

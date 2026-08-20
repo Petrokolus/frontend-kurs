d129530bffecb1f697ded705d4beea0f6b92571b
Petrokolus <peter.gisholt.dev@gmail.com>
2026-08-20 20:15:07 +0200

    feature: oppgave 1a

--- src/app/spillere/page.tsx
+++ src/app/spillere/page.tsx
@@ -2,7 +2,7 @@ export default async function SpillerePage() {
   return (
     <div className="max-w-4xl p-8">
       <div className="mb-6 flex items-center justify-between">
-        {/* Oppgave 1a - LEGG TIL EN OVERSKRIFT HER */}
+        <h1 className="text-3xl font-bold">Spillere</h1>
         {/* Oppgave 3a - LEGG TIL EN LENKE TIL /spillere/opprett HER */}
       </div>
       {/* Oppgave 1b - LEGG TIL ET SPILLERCARD HER */}

8327cdc81eb281398c14e39543aa65085d82ad2a
Petrokolus <peter.gisholt.dev@gmail.com>
2026-08-20 21:20:27 +0200

    feature: oppgave 1f

--- src/app/spillere/page.tsx
+++ src/app/spillere/page.tsx
@@ -2,33 +2,16 @@ import SpillereListe from "@/components/spillere/spillere-liste";
 import { Spiller } from "@/lib/types";
 
 export default async function SpillerePage() {
-  const mockSpillere: Spiller[] = [
-    {
-      id: 1,
-      navn: "Ola Nordmann",
-      avdeling: "Digital Engineering",
-      kull: "NK20",
-      posisjon: "Angrep",
-      rating: 100,
-      skyggerating: 100,
-    },
-    {
-      id: 2,
-      navn: "Kari Nordmann",
-      avdeling: "Design",
-      kull: "NK21",
-      posisjon: "Forsvar",
-      rating: 90,
-      skyggerating: 85,
-    },
-  ];
+  const result = await fetch("http://localhost:3000/api/spillere");
+  const spillere: Spiller[] = await result.json();
+
   return (
     <div className="max-w-4xl p-8">
       <div className="mb-6 flex items-center justify-between">
         <h1 className="text-3xl font-bold">Spillere</h1>
         {/* Oppgave 3a - LEGG TIL EN LENKE TIL /spillere/opprett HER */}
       </div>
-      <SpillereListe spillere={mockSpillere} />
+      <SpillereListe spillere={spillere} />
     </div>
   );
 }

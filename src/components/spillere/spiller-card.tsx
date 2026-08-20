5f78fcfef388943f9399405f6ed0089ddd4fdffd
Petrokolus <peter.gisholt.dev@gmail.com>
2026-08-20 21:09:42 +0200

    feature: oppgave 1e

--- src/components/spillere/spiller-card.tsx
+++ src/components/spillere/spiller-card.tsx
@@ -1,4 +1,5 @@
 import { Spiller } from "@/lib/types";
+import Image from "next/image";
 
 type Props = {
   spiller: Spiller; // Her definerer vi at props skal inneholde en spiller av typen Spiller, som vi har definert i types.ts
@@ -8,6 +9,14 @@ type Props = {
 export default function SpillerCard({ spiller }: Props) {
   return (
     <div className="flex items-center space-x-4 rounded-lg border p-4">
+      <Image
+        src={`/spiller/${spiller.id}.png`}
+        alt="Ola Nordmann"
+        width={100}
+        height={100}
+        className="aspect-square rounded-full object-cover"
+      />
+
       <div className="flex-1">
         <h2 className="text-lg font-semibold">{spiller.navn}</h2>
         <p className="text-muted-foreground text-sm">{spiller.posisjon}</p>

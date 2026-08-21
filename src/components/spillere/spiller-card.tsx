a045a5d9082735f217caa93a91ffc42f16953523
Petrokolus <peter.gisholt.dev@gmail.com>
2026-08-21 17:57:56 +0200

    feature: 2f

--- src/components/spillere/spiller-card.tsx
+++ src/components/spillere/spiller-card.tsx
@@ -1,5 +1,6 @@
 import { Spiller } from "@/lib/types";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
   spiller: Spiller; // Her definerer vi at props skal inneholde en spiller av typen Spiller, som vi har definert i types.ts
@@ -8,22 +9,24 @@ type Props = {
 
 export default function SpillerCard({ spiller }: Props) {
   return (
-    <div className="flex items-center space-x-4 rounded-lg border p-4">
-      <Image
-        src={`/spiller/${spiller.id}.png`}
-        alt="Ola Nordmann"
-        width={100}
-        height={100}
-        className="aspect-square rounded-full object-cover"
-      />
+    <Link href={`/spillere/${spiller.id}`}>
+      <div className="flex items-center space-x-4 rounded-lg border p-4">
+        <Image
+          src={`/spiller/${spiller.id}.png`}
+          alt="Ola Nordmann"
+          width={100}
+          height={100}
+          className="aspect-square rounded-full object-cover"
+        />
 
-      <div className="flex-1">
-        <h2 className="text-lg font-semibold">{spiller.navn}</h2>
-        <p className="text-muted-foreground text-sm">{spiller.posisjon}</p>
-        <p className="text-muted-foreground text-sm">
-          Rating: {spiller.rating}
-        </p>
+        <div className="flex-1">
+          <h2 className="text-lg font-semibold">{spiller.navn}</h2>
+          <p className="text-muted-foreground text-sm">{spiller.posisjon}</p>
+          <p className="text-muted-foreground text-sm">
+            Rating: {spiller.rating}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }

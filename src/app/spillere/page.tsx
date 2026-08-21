9611b498207394b2f633dd485d5f6b9a98528eee
Peter Andreas Gisholt <peter.a.gisholt@twoday.com>
2026-08-22 00:39:19 +0200

    feature: 3a

--- src/app/spillere/page.tsx
+++ src/app/spillere/page.tsx
@@ -1,15 +1,16 @@
 import SpillereListe from "@/components/spillere/spillere-liste";
 import { Spiller } from "@/lib/types";
+import Link from "next/link";
 
 export default async function SpillerePage() {
   const result = await fetch("http://localhost:3000/api/spillere");
   const spillere: Spiller[] = await result.json();
 
   return (
-    <div className="max-w-4xl p-8 mx-auto">
+    <div className="mx-auto max-w-4xl p-8">
       <div className="mb-6 flex items-center justify-between">
         <h1 className="text-3xl font-bold">Spillere</h1>
-        {/* Oppgave 3a - LEGG TIL EN LENKE TIL /spillere/opprett HER */}
+        <Link href={"/spillere/opprett"}>Opprett spiller</Link>
       </div>
       <SpillereListe spillere={spillere} />
     </div>
